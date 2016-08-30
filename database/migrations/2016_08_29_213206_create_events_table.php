e1fcbad4d09413a7998dc77886353c07c1c857df
Gavin <gavinvaught@gmail.com>
2016-08-30 09:48:45 -0500

    nothing to commit

--- database/migrations/2016_08_29_213206_create_events_table.php
+++ database/migrations/2016_08_29_213206_create_events_table.php
@@ -21,7 +21,7 @@ class CreateEventsTable extends Migration
 			$table->string('content');
 			$table->string('event_pic');
 			$table->integer('created_by');
-			$table->integer('created_by')->references('id')->on('users');
+			$table->foreign('created_by')->references('id')->on('users');
 			$table->timestamps();
 		});
     }
