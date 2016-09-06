9e59ed75440f9a4f5aa4d851c8d95f0fe62e6463
Gavin <gavinvaught@gmail.com>
2016-09-06 11:45:01 -0500

    started working on index and homecontroller

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Bar;
+use App\Event;
+use App\Feature;
+use App\Picture;
+use App\Review;
+use App\Special;
+use App\User;
+use App\Vote;
+
+class HomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data['bar'] = Bar::with('pictures', 'reviews', 'specials', 'features', 'events')->get();
+        $data['review'] = Review::with('votes', 'user', 'bar')->get();
+        $data['event'] = Event::with('bar')->get();
+        return view('index', $data);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,8 +4,54 @@
 <div class="container content">
 	<div class="row">
 		<h1>{{ Carbon\Carbon::now('America/Chicago')->formatLocalized('%A') }}  <small>{{ Carbon\Carbon::now('America/Chicago')->formatLocalized('%B %d, %Y') }}</small></h1>
-		<textarea type="text" id="startLat"></textarea>
-		<textarea type="text" id="startLon"></textarea>
+	</div>
+	<div class="row">
+		<div class="col-xs-6" id="photos">
+			<div id="carousel" class="carousel slide" data-ride="carousel">
+				<!-- Indicators -->
+				<ol class="carousel-indicators">
+					@foreach ($bar->pictures as $picture)
+					<li data-target="#carousel" data-slide-to="0" class=""></li>
+					@endforeach
+				</ol>
+				<!-- Wrapper for slides -->
+				<div class="carousel-inner carousel-image-container" role="listbox">
+					@foreach ($bar->pictures as $index => $picture)
+					<div class="item @if($index == 0) {{ 'active' }} @endif">
+						<img class="cover" src="{{ $picture->pic_url }}" alt="...">
+						<div class="carousel-caption">
+							<!-- maybe add captions to pictures table? -->
+						</div>
+					</div>
+					@endforeach
+				</div>
+				<!-- Controls -->
+				<a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
+					<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+					<span class="sr-only">Previous</span>
+				</a>
+				<a class="right carousel-control" href="#carousel" role="button" data-slide="next">
+					<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+					<span class="sr-only">Next</span>
+				</a>
+			</div>
+		</div>
 	</div>
 </div>
+@stop
+@include('partials.vote-ajax')
+@section('scripts')
+<script>
+	$('#myTabs a').click(function (e) {
+		e.preventDefault()
+		$(this).tab('show')
+	});
+	$('#myTabs a:first').tab('show');
+	$('.carousel').carousel({
+		interval: 4000
+	});
+	$('#image-upload').click(function() {
+		$('#dropzone').slideToggle('slow');
+	});
+</script>
 @stop
