ad8b9159da7e59d38171d0989f456f62f16430e2
Ernesto <neto_neto@hotmail.com>
2016-09-11 13:53:05 -0500

    Start changes on gameplans

--- resources/views/gameplans/index.blade.php
+++ resources/views/gameplans/index.blade.php
@@ -21,37 +21,7 @@
                         <a href="/users/{{ $gameplan->author->id }}">
                             <h5>{{ $gameplan->author->first_name . ' ' . $gameplan->author->formatLastName() }}</h5></a>
                         {{--<h2>{{ $gameplan->date()->format('l, F jS Y') }}</h2>--}}
-                        <h4>Hop-Stops:</h4>
-                        <div class="row">
-                            <div class="col-xs-8 col-xs-offset-2" id="photos">
-                                <div id="carousel" class="carousel slide" data-ride="carousel">
-                                    <!-- Wrapper for slides -->
-                                    <div class="carousel-inner carousel-image-container events-slider" role="listbox">
-                                        @foreach($gameplan->bars as $index => $gpbar)
-                                            <div data-value="{{ $gpbar->bar->id }}"
-                                                 class="item @if($index == 0) {{ 'active' }} @endif">
-                                                <div class="carousel-caption">
-                                                    <h2>{{ $gpbar->bar->name }}</h2>
-                                                    <p>{{ $index+1 }}</p>
-                                                </div>
-                                                <img class="cover"
-                                                     src="{{ $gpbar->bar->pictures->first()->pic_url or '' }}"
-                                                     alt="{{ $gpbar->bar->name }}">
-                                            </div>
-                                        @endforeach
-                                    </div>
-                                    <!-- Controls -->
-                                    <a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
-                                        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                                        <span class="sr-only">Previous</span>
-                                    </a>
-                                    <a class="right carousel-control" href="#carousel" role="button" data-slide="next">
-                                        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                                        <span class="sr-only">Next</span>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
+
                         <h4>Hoppers:</h4>
                         @foreach($gameplan->hoppers as $hopper)
                             <h5>{{ $hopper->user->first_name . ' ' . $hopper->user->formatLastName() }}</h5>
